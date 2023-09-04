5d2b56037a0fa1b036a6a1fe749130b865be9730
Laleti Cynthia <+>
2023-09-04 16:55:12 +0300

    code alog

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => onDeleteItem(item));
+  }
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
@@ -8,9 +15,10 @@ function Item({ item }) {
       <button className={item.isInCart ? "remove" : "add"}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
+
 
 export default Item;
